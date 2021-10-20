2dfc6ac0e7ed12499964e4af17c3f37cbb919789
Abayomi Aishida <yomicorperate@gmail.com>
2021-10-20 21:02:08 +0100

    Handling events with event binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,6 +8,7 @@ export class PorductListComponent {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 5;
+  showImage = false
   products: any[] = [
     {
       "productId": 1,
@@ -60,4 +61,8 @@ export class PorductListComponent {
       "imageUrl": "assets/images/xbox-controller.png"
     }
   ]
+
+  toggleImage(): void {
+    this.showImage = !this.showImage
+  }
 }
