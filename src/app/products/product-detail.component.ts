3f3cb999ee5fb4e21dc29abbb16e3d5f2ea43ec4
Abayomi Aishida <yomicorperate@gmail.com>
2021-10-27 20:58:17 +0100

    Activate routes with code and protect routes with guard service

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './products';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detaile'
+  pageTitle: string = 'Product Detail';
+  product: IProduct | undefined
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.pageTitle +=  ` ${id}`
+    this.product = {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products'])
   }
 
 }
